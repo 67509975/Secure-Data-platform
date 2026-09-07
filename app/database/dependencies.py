c3186cc97db32e0c5fb39c57e02ab1f6b38fb729
Tshego <tskhojhb025@student.wethinkcode.co.za>
2026-09-07 13:28:29 +0200

    feat: add database session dependency

---
+++ app/database/dependencies.py
@@ -0,0 +1,11 @@
+from collections.abc import Generator
+from sqlalchemy.orm import Session
+from app.database.database import SessionLocal
+
+def get_db():
+    db = SessionLocal
+
+    try:
+        yield db
+    finally:
+        db.close()
