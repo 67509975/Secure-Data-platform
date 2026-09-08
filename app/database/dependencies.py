5b6599c8ae2701c24aad2d2945c23ea35f9a1628
Tshego <tskhojhb025@student.wethinkcode.co.za>
2026-09-08 11:04:27 +0200

    Feat: implememnt secure user registration

--- app/database/dependencies.py
+++ app/database/dependencies.py
@@ -1,9 +1,12 @@
 from collections.abc import Generator
+
 from sqlalchemy.orm import Session
+
 from app.database.database import SessionLocal
 
-def get_db():
-    db = SessionLocal
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
 
     try:
         yield db
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
+
 from app.core.security import hash_password
 from app.models.user import User
 from app.schemas.user import UserCreate
 
-def create_user(db:Session, user_data: UserCreate):
-    hashed_password = hashed_password(user_data.password)
+
+def create_user(db: Session, user_data: UserCreate) -> User:
+    hashed_password = hash_password(user_data.password)
 
     user = User(
         username=user_data.username,
