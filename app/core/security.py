61f791fe8c096f13245e7c34e869a4b9bb93629f
Tshego <tskhojhb025@student.wethinkcode.co.za>
2026-09-08 10:02:40 +0200

    Feat: add ARgon2 password hashing

---
+++ app/core/security.py
@@ -0,0 +1,8 @@
+from pwdlib import PasswordHash
+
+Password_hash = PasswordHash.recommended()
+def hash_password(password: str):
+    return Password_hash.hash(password)
+
+def verify_password(password: str, hashed_password:str):
+    return password_hash.verify(password, hashed_password)
